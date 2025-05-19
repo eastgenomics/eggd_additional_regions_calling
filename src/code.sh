81f15ca0b7bd57095703d17bc35d4e1bccff246f
nadia31415 <nadia.mohammed@addenbrookes.nhs.uk>
2025-05-19 16:09:07 +0100

    added read depth thresholds

--- src/code.sh
+++ src/code.sh
@@ -79,7 +79,7 @@ _generate_region_vcfs() {
             -r "$chromPos" -a FORMAT/AD,FORMAT/DP -Ou | \
             bcftools call -mv -Oz -o "$output_vcf"
 
-        bcftools view -i "REF=='${knownRef}' && ALT=='${knownAlt}'" -Oz -o "${output_vcf}_filtered.vcf.gz" "$output_vcf"
+        bcftools view -i "REF=='${knownRef}' && ALT=='${knownAlt}' && FORMAT/DP>${read_depth_threshold}" -Oz -o "${output_vcf}_filtered.vcf.gz" "$output_vcf"
         mv "${output_vcf}_filtered.vcf.gz" "$output_vcf"
         _index_vcf_if_missing "$output_vcf"
 
